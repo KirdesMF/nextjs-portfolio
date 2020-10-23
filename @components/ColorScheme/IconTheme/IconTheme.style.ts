40fede50f8aeca1f3c4d211b90d72da95fc755a1
Cedric Gourville <cedgourville@gmail.com>
2020-10-23 02:46:07 +0200

    change color icon dark mode

--- @components/ColorScheme/IconTheme/IconTheme.style.ts
+++ @components/ColorScheme/IconTheme/IconTheme.style.ts
@@ -1,10 +1,11 @@
 import { css } from 'linaria';
 import THEME from 'Theme/theme';
 
-const svg = css`
+export const svg = css`
    width: 5em;
-   filter: drop-shadow(0 0 5px ${THEME.COLORS['grey-100']});
-   transition: transform 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+   filter: drop-shadow(0 0 5px black);
+   transition: transform 500ms cubic-bezier(0.175, 0.885, 0.32, 1.275),
+      fill 0.5s ease-in-out;
 
    &:hover,
    &:focus {
@@ -12,6 +13,45 @@ const svg = css`
    }
 `;
 
-export default {
-   svg,
-};
+export const earth = css`
+   & > path:nth-of-type(1) {
+      fill: ${THEME.COLORS.background};
+   }
+
+   & > path:nth-of-type(2),
+   & > path:nth-of-type(3) {
+      fill: ${THEME.COLORS['primary-200']};
+   }
+`;
+
+export const cloud = css`
+   & path {
+      fill: ${THEME.COLORS['primary-600']};
+   }
+`;
+
+export const sun = css`
+   & > path {
+      fill: ${THEME.COLORS['secondary-600']};
+   }
+
+   & > circle {
+      fill: ${THEME.COLORS['secondary-300']};
+   }
+`;
+
+export const moon = css`
+   fill: ${THEME.COLORS['secondary-600']};
+`;
+
+export const stars = css`
+   & circle {
+      fill: ${THEME.COLORS['primary-600']};
+   }
+`;
+
+export const line = css`
+   fill: none;
+   stroke: ${THEME.COLORS['secondary-300']};
+   stroke-width: 1;
+`;
